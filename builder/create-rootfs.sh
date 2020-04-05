44e74163919f49998c691956ffaba37d564ea3df
Ezekiel Bethel <zek@9net.org>
2020-04-05 23:56:36 +0100

    update for /boot being sd root

--- builder/create-rootfs.sh
+++ builder/create-rootfs.sh
@@ -5,7 +5,6 @@ root_dir="$(dirname "$(dirname "$(readlink -fm "$0")")")"
 cleanup(){
 	umount -R ${root_dir}/tmp/mnt/*
 	umount -R ${root_dir}/tmp/*
-	kpartx -d ${root_dir}/l4t-arch.img
 	rm -rf ${root_dir}/tmp/
 }
 
@@ -13,7 +12,6 @@ prepare() {
 	mkdir -p ${root_dir}/tarballs/
 	mkdir -p ${root_dir}/tmp/arch-bootfs/
 	mkdir -p ${root_dir}/tmp/arch-rootfs/pkgs
-	mkdir -p ${root_dir}/tmp/mnt/bootfs/
 	mkdir -p ${root_dir}/tmp/mnt/rootfs/
 
 	if [[ ! -e ${root_dir}/tarballs/ArchLinuxARM-aarch64-latest.tar.gz ]]; then
@@ -30,15 +28,18 @@ prepare() {
 
 setup_base(){
 	cp ${root_dir}/builder/build-stage2.sh ${root_dir}/builder/base-pkgs ${root_dir}/tmp/arch-rootfs/
-	cp -r ${root_dir}/pkgbuilds/*.pkg.* ${root_dir}/tmp/arch-rootfs/pkgs/
+	cp -r ${root_dir}/pkgbuilds/*/*.pkg.* ${root_dir}/tmp/arch-rootfs/pkgs/
 	
 	bsdtar xpf ${root_dir}/tarballs/ArchLinuxARM-aarch64-latest.tar.gz -C ${root_dir}/tmp/arch-rootfs/
+
+	# Workaround for flakiness of `pt` mirror.
+	sed -i 's/mirror.archlinuxarm.org/de.mirror.archlinuxarm.org/g' ${root_dir}/tmp/arch-rootfs/etc/pacman.d/mirrorlist
 
 	echo "[switch]
 SigLevel = Optional
 Server = https://9net.org/l4t-arch/" >> ${root_dir}/tmp/arch-rootfs/etc/pacman.conf
 
-	echo -e "/dev/mmcblk0p1	/mnt/hos_data	vfat	rw,relatime	0	2\n/boot /mnt/hos_data/l4t-arch/	none	bind	0	0" >> ${root_dir}/tmp/arch-rootfs/etc/fstab
+	echo -e "/dev/mmcblk0p1	/boot	vfat	rw,relatime	0	2\n" >> ${root_dir}/tmp/arch-rootfs/etc/fstab
 
 	cp /usr/bin/qemu-aarch64-static ${root_dir}/tmp/arch-rootfs/usr/bin/
 	cp /etc/resolv.conf ${root_dir}/tmp/arch-rootfs/etc/
@@ -57,25 +58,22 @@ Server = https://9net.org/l4t-arch/" >> ${root_dir}/tmp/arch-rootfs/etc/pacman.c
 buildimg(){
 	size=$(du -hs ${root_dir}/tmp/arch-rootfs/ | head -n1 | awk '{print int($1+2);}')$(du -hs ${root_dir}/tmp/arch-rootfs/ | head -n1 | awk '{print $1;}' | grep -o '[[:alpha:]]')
 
+	rm ${root_dir}/l4t-arch.img
 	dd if=/dev/zero of=${root_dir}/l4t-arch.img bs=1 count=0 seek=$size
 	
-	parted ${root_dir}/l4t-arch.img --script -- mklabel msdos
-	parted -a optimal ${root_dir}/l4t-arch.img mkpart primary 0% 476MB
-	parted -a optimal ${root_dir}/l4t-arch.img mkpart primary 476MB 100%
-	
-	loop_dev=$(kpartx -av ${root_dir}/l4t-arch.img | grep -oh "\w*loop\w*")
+	loop=`losetup --find`
+	losetup $loop ${root_dir}/l4t-arch.img
 
-	loop1=`echo "${loop_dev}" | head -1`
-	loop2=`echo "${loop_dev}" | tail -1`
+	mkfs.ext4 $loop
+	mount $loop ${root_dir}/tmp/mnt/rootfs/
 
-	mkfs.fat -F 32 /dev/mapper/${loop1}
-	mkfs.ext4 /dev/mapper/${loop2}
-
-	mount -o loop /dev/mapper/${loop1} ${root_dir}/tmp/mnt/bootfs/
-	mount -o loop /dev/mapper/${loop2} ${root_dir}/tmp/mnt/rootfs/
-	
-	cp -r ${root_dir}/tmp/arch-bootfs/* ${root_dir}/tmp/mnt/bootfs/
 	cp -pdr ${root_dir}/tmp/arch-rootfs/* ${root_dir}/tmp/mnt/rootfs/
+	umount $loop
+	losetup -d $loop
+
+	pushd ${root_dir}/tmp/arch-bootfs
+	zip -r ${root_dir}/l4t-boot.zip *
+	popd
 }
 
 if [[ `whoami` != root ]]; then
